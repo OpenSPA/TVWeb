62ef1f65dbd1301d3fcf2b6d84d2abd2e4ebcd43
morser <morser123@hotmail.com>
2013-07-19 13:15:59 +0200

    Append Cinetux & SeriesYonkys modules

--- usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/cinetux.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/cinetux.py
@@ -30,11 +30,11 @@ def mainlist(item):
     itemlist = []
     itemlist.append( Item(channel=__channel__ , action="peliculas", title="Destacadas o actualizadas" , url="http://www.cinetux.org/destacados", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
     itemlist.append( Item(channel=__channel__ , action="peliculas", title="Novedades" , url="http://www.cinetux.org/", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
-    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades subtitulado" , url="http://www.cinetux.org/", extra="Nuevo Sub", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
-    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades DVD" , url="http://www.cinetux.org/", extra="ltimo DVDRIP", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
-    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades latino" , url="http://www.cinetux.org/", extra="Nuevo Latino", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
-    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades castellano" , url="http://www.cinetux.org/", extra="Nuevo Castellano", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
-    itemlist.append( Item(channel=__channel__ , action="bloque", title="Nueva calidad disponible" , url="http://www.cinetux.org/", extra="Nueva Calidad", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
+    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades subtitulado" , url="http://www.adsctx.net/sub.html", extra="Nuevo Sub", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
+    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades DVD" , url="http://www.adsctx.net/dvdrip.html", extra="ltimo DVDRIP", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
+    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades latino" , url="http://www.adsctx.net/latino.html", extra="Nuevo Latino", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
+    itemlist.append( Item(channel=__channel__ , action="bloque", title="Novedades castellano" , url="http://www.adsctx.net/castellano.html", extra="Nuevo Castellano", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
+    itemlist.append( Item(channel=__channel__ , action="bloque", title="Nueva calidad disponible" , url="http://www.adsctx.net/calidad.html", extra="Nueva Calidad", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
     itemlist.append( Item(channel=__channel__ , action="generos", title="Por géneros" , url="http://www.cinetux.org/", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" ))
 
     return itemlist
@@ -75,37 +75,33 @@ def peliculas(item):
 
 
     '''
-    <!--PELICULA--></p>
     <div class="peli_item textcenter">
-    <div class="pelicula_img">
-    <a href="http://www.cinetux.org/2013/02/ver-pelicula-red-dawn-amanecer-rojo-online-gratis-2012.html">
-    <img alt="" src="http://1.bp.blogspot.com/-TuZFuLOtYlE/ULWlFNDJYZI/AAAAAAAAAfY/rZ1k2-Kebm8/s270/red-dawn-poster1.jpg" width="134" height="193" /></a></div>
+    <div class="pelicula_img"><a href="http://www.cinetux.org/2013/01/ver-pelicula-la-matanza-de-texas-3d-online-gratis-2013.html"><img alt="" src="http://1.bp.blogspot.com/-Rg9cCqo9Akg/UWub17Y4jVI/AAAAAAAAFo0/jp-8bQVxTB4/s200/La+Masacre+De+Texas+3D.jpg" width="104" height="150" /></a></div>
     <div class="dvdrip"> </div>
-    <p><a href="http://www.cinetux.org/2013/02/ver-pelicula-red-dawn-amanecer-rojo-online-gratis-2012.html">
-    <span style="color: white; font-size: 13px;">Red Dawn<br />(Amanecer Rojo)</span></a><br /> <span class="rosa">DVD-RIP</span><span class="icos_lg"><br /> <img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-sBVn6JyvTeA/TeA7nERPF4I/AAAAAAAADM4/2iRloiVHCG8/lat.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/--2xClYCIiwY/TeA7nUAYXXI/AAAAAAAADNE/Mr530W5fFFk/sub.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-t8w6a8_Hk-w/TeA7nd5Ad9I/AAAAAAAADNI/UYV40sR_sfc/online.png" /><img style="border: 0pt none;" alt="" src="https://lh5.googleusercontent.com/-35yef7ubBv8/TeA7nNfUXJI/AAAAAAAADM0/RCQqAiWLX9o/descarga.png" /> </span></p>
-
-    <p><!--FIN PELICULA--><!--PELICULA--></p>
-    <div class="peli_item textcenter">
-    <div class="pelicula_img">
-    <a href="http://www.cinetux.org/2013/02/ver-pelicula-hermosas-criaturas-online-gratis-2013.html">
-    <img alt="" src="http://2.bp.blogspot.com/-K5OrpjDrhhc/UQZiWg0OanI/AAAAAAAACw4/qHjVfRwOyj0/s270/hermosas+criaturas.jpg" width="134" height="193" /></a></div>
-    <div class="estreno"> </div>
-    <p><a href="http://www.cinetux.org/2013/02/ver-pelicula-hermosas-criaturas-online-gratis-2013.html">
-    <span style="color: white; font-size: 13px;">Hermosas Criaturas</span></a><br /> <span class="rosa">TS-SCREENER</span><span class="icos_lg"><br /> <img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-d-kbbyjdxYI/TeA7nHuZ8mI/AAAAAAAADM8/Ii149s3ed2c/espanol.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-sBVn6JyvTeA/TeA7nERPF4I/AAAAAAAADM4/2iRloiVHCG8/lat.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/--2xClYCIiwY/TeA7nUAYXXI/AAAAAAAADNE/Mr530W5fFFk/sub.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-t8w6a8_Hk-w/TeA7nd5Ad9I/AAAAAAAADNI/UYV40sR_sfc/online.png" /><img style="border: 0pt none;" alt="" src="https://lh5.googleusercontent.com/-35yef7ubBv8/TeA7nNfUXJI/AAAAAAAADM0/RCQqAiWLX9o/descarga.png" /> </span></p>
+    <p><span class="rosa">DVD-RIP</span><br /><span class="icos_lg"><img style="border: 0pt none;" alt="" src="http://4.bp.blogspot.com/-qVqs0f0dsoM/UVJ2-nPN6MI/AAAAAAAAB_8/NkYdkmM-uvY/s320/lat.png" /><img style="border: 0pt none;" alt="" src="http://3.bp.blogspot.com/-t8w6a8_Hk-w/TeA7nd5Ad9I/AAAAAAAADNI/UYV40sR_sfc/s16/online.png" /><img style="border: 0pt none;" alt="" src="https://lh5.googleusercontent.com/-35yef7ubBv8/TeA7nNfUXJI/AAAAAAAADM0/RCQqAiWLX9o/s16/descarga.png" /></span></p>
+    <div class="calidad5"> </div>
+    <p>&nbsp;</p>
+    </div>
     '''
 
-    patron  = '<!--PELICULA--></p[^<]+'
-    patron += '<div class="peli_item textcenter"[^<]+'
+    patron  = '<div class="peli_item textcenter"[^<]+'
     patron += '<div class="pelicula_img"[^<]+'
     patron += '<a href="([^"]+)[^<]+<img alt="" src="([^"]+)".*?'
-    patron += '<span style="color[^>]+>(.*?)</span></a><br[^<]+'
     patron += '<span class="rosa">(.*?)</span>'
     matches = re.compile(patron,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
-    for scrapedurl,scrapedthumbnail,title,calidad in matches:
+    for scrapedurl,scrapedthumbnail,calidad in matches:
+        partes = scrapedurl.split("/")
+        titulo = partes[len(partes)-1]
+        titulo = titulo.replace("ver-pelicula","")
+        titulo = titulo.replace("online-gratis","")
+        titulo = titulo.replace(".html","")
+        titulo = titulo.replace("-"," ")
+        titulo = titulo.strip().capitalize()
+
         scrapedplot = ""
-        scrapedtitle = title + " ["+calidad+"]"
+        scrapedtitle = titulo + " ["+calidad+"]"
         if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
         itemlist.append( Item(channel=__channel__, action="findvideos", title=scrapedtitle , fulltitle=scrapedtitle, url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , viewmode="movie", fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg",  folder=True) )
 
@@ -139,9 +135,10 @@ def bloque(item):
 
     # Descarga la página
     data = scrapertools.cachePage(item.url)
-    data = scrapertools.get_match(data,item.extra+'</h6>(.*?)</div>')
+    #data = scrapertools.get_match(data,item.extra+'</h6>(.*?)</div>')
     
-    patron = '<a href="([^"]+)"><img style="[^"]+" src="([^"]+)"'
+    #<a target="_blank" href="http://www.cinetux.org/2013/04/ver-pelicula-dark-skies-online-gratis-2013.html"><img style="border:1px solid #FDC101;" src="http://4.bp.blogspot.com/-UlKHsLS3Tsk/URJotTqg-_I/AAAAAAAAA5c/8lhe3kY4jzc/s80/Dark+Skies+%282013%29+Movie+Review.jpg" height="75" width="47">
+    patron = '<a target="_blank" href="([^"]+)"><img style="[^"]+" src="([^"]+)"'
     matches = re.compile(patron,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
 
@@ -240,7 +237,8 @@ def findvideos(item):
     patron += '<td class="server-td[^>]+>([^<]+)</td>[^<]+'
     patron += '<td class="idioma-td[^>]+>([^<]+)</td>[^<]+'
     patron += '<td class="fuente-td[^>]+>([^<]+)</td>[^<]+'
-    patron += '<td class="link-td">(.*?)</td>'
+    #patron += '<td class="link-td">(.*?)</td>'
+    patron += '<td class="link-td"><span><a rel="nofollow" target="_blank" class="myButtonLink" href="([^"]+)"'
     matches = re.compile(patron,re.DOTALL).findall(data)
     scrapertools.printMatches(matches)
     for scrapedtitle,scrapedserver,scrapedlanguage,scrapedquality,scrapedlink in matches:
@@ -248,7 +246,7 @@ def findvideos(item):
         url = scrapedlink
         thumbnail = item.thumbnail
         plot = ""
-        itemlist.append( Item(channel=__channel__, action="play", title=title , fulltitle=item.fulltitle+" ["+scrapedlanguage+"]["+scrapedquality+"]", url=url , thumbnail=thumbnail , plot=plot , fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" , folder=False) )
+        itemlist.append( Item(channel=__channel__, action="play", title=title , fulltitle=item.fulltitle+" ["+scrapedlanguage+"]["+scrapedquality+"]", url=url , thumbnail=thumbnail , plot=plot , fanart="http://pelisalacarta.mimediacenter.info/fanart/cinetux.jpg" , server = scrapedserver, folder=False) )
 
     patron  = '<tr class="tabletr">[^<]+'
     patron += '<td class="episode-server[^>]+><img[^>]+>([^>]+)</td>[^<]+'
@@ -263,7 +261,7 @@ def findvideos(item):
         url = scrapedlink
         thumbnail = item.thumbnail
         plot = ""
-        itemlist.append( Item(channel=__channel__, action="play", title=title , fulltitle=item.fulltitle+" ["+scrapedlanguage+"]["+scrapedquality+"]", url=url , thumbnail=thumbnail , plot=plot , folder=False) )
+        itemlist.append( Item(channel=__channel__, action="play", title=title , fulltitle=item.fulltitle+" ["+scrapedlanguage+"]["+scrapedquality+"]", url=url , thumbnail=thumbnail , plot=plot , folder=False ) )
 
     if len(itemlist)==0:
         itemlist = servertools.find_video_items(data=data)
