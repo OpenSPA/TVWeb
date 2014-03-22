586d1a6b2001ba9c98aff2c88bfb4c4bef15aeb6
morser <morser123@hotmail.com>
2014-03-22 16:29:40 +0100

    [pelis24] Update module

--- usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/pelis24.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/pelis24.py
@@ -25,63 +25,91 @@ def isGeneric():
     return True
 
 def mainlist(item):
-    logger.info("[pelis24.py] mainlist")
+    logger.info("channels.pelis24 mainlist")
 
     itemlist = []
-    itemlist.append( Item(channel=__channel__, title="Novedades"     , action="peliculas", url="http://pelis24.com/index.php"))
-    itemlist.append( Item(channel=__channel__, title="Estrenos" , action="peliculas", url="http://pelis24.com/estrenos/"))
-    itemlist.append( Item(channel=__channel__, title="Recientes" , action="peliculas", url="http://pelis24.com/index.php?do=lastnews"))
-    itemlist.append( Item(channel=__channel__, title="HD 720p" , action="peliculas", url="http://pelis24.com/hd/"))
-    itemlist.append( Item(channel=__channel__, title="HD 480p" , action="peliculas", url="http://pelis24.com/peliculas480p/"))
-    itemlist.append( Item(channel=__channel__, title="Peliculas en Castellano" , action="peliculas", url="http://pelis24.com/pelicula-ca/"))
-    itemlist.append( Item(channel=__channel__, title="Peliculas 3D" , action="peliculas", url="http://pelis24.com/pelicula-3d"))
+    itemlist.append( Item(channel=__channel__, title="Novedades"              , action="peliculas" , url="http://pelis24.com/index.php"))
+    itemlist.append( Item(channel=__channel__, title="Listado por categorías" , action="categorias", url="http://pelis24.com/index.php", extra="CATEGORIAS"))
+    itemlist.append( Item(channel=__channel__, title="Listado por calidades"  , action="categorias", url="http://pelis24.com/index.php", extra="CALIDADES"))
+    itemlist.append( Item(channel=__channel__, title="Listado por idiomas"    , action="categorias", url="http://pelis24.com/index.php", extra="IDIOMAS"))
     return itemlist
 
 def peliculas(item):
-    logger.info("[pelis24.py] peliculas")
+    logger.info("channels.pelis24 peliculas")
+    itemlist = []
+
+    # Descarga la pagina
+    data = scrapertools.cachePage(item.url)
+
+    # Limita el bloque donde buscar
+    data = scrapertools.find_single_match(data,"<div id='dle-content'>(.*?<div class=\"navigation\">.*?)</div[^<]+</div[^<]+</div>")
+
+    # Extrae los items    
+    patron  = '<div class="movie_box">(.*?)<div class="postbottom">'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+
+    for bloque in matches:
+        scrapedtitle = scrapertools.find_single_match(bloque,"<h3>([^<]+)</h3>")
+        scrapedurl = scrapertools.find_single_match(bloque,'<a href="([^"]+)"><img class="homethumb"')
+        scrapedthumbnail = scrapertools.find_single_match(bloque,'<img class="homethumb" style="[^"]+" data-cfsrc="([^"]+)"')
+        scrapedplot = scrapertools.find_single_match(bloque,'<span class="pop_desc">(.*?)</span>')
+
+        title = unicode( scrapedtitle, "iso-8859-1" , errors="replace" ).encode("utf-8")
+        url = scrapedurl
+        thumbnail = scrapedthumbnail.strip()
+        plot = unicode( scrapedplot, "iso-8859-1" , errors="replace" ).encode("utf-8").strip()
+
+        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
+        itemlist.append( Item(channel=__channel__, action="findvideos", title=title , url=url , thumbnail=thumbnail , plot=plot , viewmode="movie_with_plot", folder=True) )
+
+    # Extrae el paginador
+    next_page = scrapertools.find_single_match(data,"<a href=\"([^\"]+)\"><spam class='last'>")
+    if next_page!="":
+        itemlist.append( Item(channel=__channel__, action="peliculas", title=">> Pagina siguiente" , url=next_page , folder=True) )
+
+    return itemlist
+
+def categorias(item):
+    logger.info("channels.pelis24 categorias")
     itemlist = []
 
     # Descarga la página
     data = scrapertools.cachePage(item.url)
-    
-    bloque = scrapertools.get_match(data,"<div id='dle-content'>(.*?<span class=\"thide pnext\">Siguiente</span></a>)")
-    #<a href="http://www.pelis24.com/peliculas/11010-fast-furious-5-a-todo-gas-5-espanol-online.html">
-    #<img style="display:none;visibility:hidden;" data-cfsrc="http://imgs24.com/images/l15963435d.jpg" width="145" height="211" alt="Rápidos y furiosos 5 / A todo gas 5 (2011)" title="Rápidos y furiosos 5 / A todo gas 5 (2011)"/>
-    #<noscript><img src="http://imgs24.com/images/l15963435d.jpg" width="145" height="211" alt="Rápidos y furiosos 5 / A todo gas 5 (2011)" title="Rápidos y furiosos 5 / A todo gas 5 (2011)"/></noscript></a>&nbsp;&nbsp;
+    logger.info("data="+data)
 
-    patron = '<a href="([^"]+)"[^<]+<img[^<]+<noscript[^<]+<img src="([^"]+)" width="[^"]+" height="[^"]+" alt="[^"]+" title="([^"]+)"'
+    # Limita el bloque donde buscar
+    data = scrapertools.find_single_match(data,'<b>'+item.extra+'</b>(.*?)</ul>')
+    logger.info("data="+data)
 
-    matches = re.compile(patron,re.DOTALL).findall(bloque)
-    scrapertools.printMatches(matches)
+    # Extrae las entradas
+    patron = '<a href="([^"]+)"><b>([^<]+)</b></a>'
+    matches = re.compile(patron,re.DOTALL).findall(data)
+    if (DEBUG): scrapertools.printMatches(matches)
+                                          
+    for scrapedurl,scrapedtitle in matches:
+        url = urlparse.urljoin(item.url,scrapedurl)
+        if url=="http://pelis24.com/movies/bluray/":
+            url = "http://pelis24.com/pelicula-3d/"
 
-    for scrapedurl,scrapedthumbnail,scrapedtitle in matches:
-        scrapedplot = ""
-        title = unicode( scrapedtitle, "iso-8859-1" , errors="replace" ).encode("utf-8")
+        if url=="http://pelis24.com/movies/dvdrip/":
+            url = "http://pelis24.com/peliculas480p/"
 
-        if (DEBUG): logger.info("title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"]")
-        itemlist.append( Item(channel=__channel__, action="findvideos", title=title , url=scrapedurl , thumbnail=scrapedthumbnail , plot=scrapedplot , folder=True) )
+        title = scrapedtitle.strip()
+        thumbnail = ""
+        plot = ""
+        if (DEBUG): logger.info("title=["+title+"], url=["+url+"], thumbnail=["+thumbnail+"]")
 
-
-    # Extrae el paginador
-    patronvideos  = '<a href="([^"]+)"><span class="thide pnext">Siguiente</span></a>'
-    
-    matches = re.compile(patronvideos,re.DOTALL).findall(data)
-    scrapertools.printMatches(matches)
-
-    if len(matches)>0:
-        scrapedurl = urlparse.urljoin(item.url,matches[0])
-        itemlist.append( Item(channel=__channel__, action="peliculas", title="Página siguiente >>" , url=scrapedurl , folder=True) )
+        itemlist.append( Item(channel=__channel__, action="peliculas", title=title , url=url , thumbnail=thumbnail , plot=plot , folder=True) )
 
     return itemlist
 
-
-# Verificación automática de canales: Esta función debe devolver "True" si está ok el canal.
+# Verificacion automatica de canales: Esta funcion debe devolver "True" si esta ok el canal.
 def test():
     from servers import servertools
     
     # mainlist
     mainlist_items = mainlist(Item())
-    # Da por bueno el canal si alguno de los vídeos de "Novedades" devuelve mirrors
+    # Da por bueno el canal si alguno de los videos de "Novedades" devuelve mirrors
     novedades_items = peliculas(mainlist_items[0])
     bien = False
     for novedades_item in novedades_items:
