a6d45a1ac9af054a8a2f91f184d75fb407d13c98
morser <morser123@hotmail.com>
2015-02-15 13:01:30 +0100

    [clantve] update channel

--- usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/clantve.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVweb/TVweb/channels/clantve.py
@@ -9,6 +9,7 @@ import urlparse,re
 from core import logger
 from core import scrapertools
 from core.item import Item
+from core import jsontools
 
 DEBUG = True
 CHANNELNAME = "clantve"
@@ -20,9 +21,8 @@ def mainlist(item):
     logger.info("tvalacarta.channels.clantv mainlist")
 
     itemlist = []
-    itemlist.append( Item(channel=CHANNELNAME, title="Últimos vídeos añadidos" , url="http://www.rtve.es/infantil/components/TE_INFDEF/videos/videos-1.inc" , action="ultimos_videos" , folder=True) )
-    itemlist.append( Item(channel=CHANNELNAME, title="Todos los programas" , url="http://www.rtve.es/infantil/series/" , action="programas" , folder=True) )
-
+    #itemlist.append( Item(channel=CHANNELNAME, title="Últimos vídeos añadidos" , url="http://www.rtve.es/infantil/components/TE_INFDEF/videos/videos-1.inc" , action="ultimos_videos" , folder=True) )
+    itemlist.append( Item(channel=CHANNELNAME, title="Todos los programas" , url="http://www.rtve.es/api/agr-programas/490/programas.json?size=60&page=1" , action="programas" , folder=True) )
     return itemlist
 
 def programas(item):
@@ -32,73 +32,30 @@ def programas(item):
 
     # Descarga la página
     data = scrapertools.cache_page(item.url)
+    logger.info("data="+data)
+    json_object = jsontools.load_json(data)
+    logger.info("json_object="+repr(json_object))
+    json_items = json_object["page"]["items"]
 
-    # Extrae los programas
-    patron  = '<div class="informacion-serie">[^<]+'
-    patron += '<h3>[^<]+'
-    patron += '<a href="([^"]+)">([^<]+)</a>[^<]+'
-    patron += '</h3>[^<]+'
-    patron += '<a[^>]+>[^<]+</a><img.*?src="([^"]+)"><div>(.*?)</div>'
-
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-
-    for match in matches:
-        scrapedtitle = match[1]
-        scrapedtitle = scrapertools.unescape(scrapedtitle)
-        scrapedurl = urlparse.urljoin(item.url,match[0])
-        scrapedurl = urlparse.urljoin(scrapedurl,"videos")
-        scrapedthumbnail = urlparse.urljoin(item.url,match[2])
-        scrapedplot = match[3]
-        scrapedplot = scrapertools.unescape(scrapedplot).strip()
-        scrapedplot = scrapertools.htmlclean(scrapedplot).strip()
-
-        scrapedpage = urlparse.urljoin(item.url,match[0])
-        if (DEBUG): logger.info("scraped title=["+scrapedtitle+"], url=["+scrapedurl+"], thumbnail=["+scrapedthumbnail+"] plot=["+scrapedplot+"]")
-        #logger.info(scrapedplot)
-
-        # Añade al listado
-        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="episodios" , url=scrapedurl, thumbnail=scrapedthumbnail, plot=scrapedplot , page=scrapedpage, show=scrapedtitle , folder=True) )
+    for json_item in json_items:
+        title = json_item["name"]
+        url = json_item["uri"]
+        thumbnail = json_item["imgPoster"]
+        if json_item["description"] is not None:
+            plot = json_item["description"]
+        else:
+            plot = ""
+        fanart = json_item["imgPortada"]
+        page = url
+        if (DEBUG): logger.info(" title=["+repr(title)+"], url=["+repr(url)+"], thumbnail=["+repr(thumbnail)+"] plot=["+repr(plot)+"]")
+        itemlist.append( Item(channel=CHANNELNAME, title=title , action="episodios" , url=url, thumbnail=thumbnail, plot=plot , page=page, show=title , fanart=fanart, viewmode="movie_with_plot", folder=True) )
 
     # Añade el resto de páginas
-    next_page = scrapertools.find_single_match(data,'<li class="siguiente">[^<]+<a rel="next" title="Ir a la p&aacute;gina siguiente" href="([^"]+)">Siguiente')
-    if next_page!="":
-        itemlist.append(Item(channel=CHANNELNAME,action="programas",title=">> Página siguiente",url=urlparse.urljoin(item.url,next_page), folder=True))
+    current_page = scrapertools.find_single_match(item.url,'page=(\d+)')
+    next_page = str( int(current_page)+1 )
+    itemlist.append(Item(channel=CHANNELNAME,action="programas",title=">> Página siguiente",url=item.url.replace("page="+current_page,"page="+next_page), folder=True))
 
     return itemlist
-
-def detalle_programa(item):
-    
-    #http://www.rtve.es/infantil/series/monsuno/videos/
-    #http://www.rtve.es/infantil/series/hay-nuevo-scooby-doo/
-    url = item.url
-    if url.endswith("/videos"):
-        url = url.replace("/videos","")
-    
-    # Descarga la página
-    print "url="+url
-    data = scrapertools.cache_page(url)
-    data = scrapertools.get_match(data,'<div class="contenido-serie">(.*?</div>)')
-    print "data="+data
-
-    # Obtiene el thumbnail
-    try:
-        item.thumbnail = scrapertools.get_match(data,'<img.*?src="([^"]+)"')
-    except:
-        pass
-
-    try:
-        item.plot = scrapertools.htmlclean( scrapertools.get_match(data,'<div>(.*?)</div>') ).strip()
-    except:
-        pass
-
-    try:
-        title = scrapertools.get_match(data,'<h3>[^<]+<a[^>]+>([^<]+)</a>[^<]+</h3>').strip()
-        item.title = scrapertools.entityunescape(title)
-    except:
-        pass
-
-    return item
 
 def episodios(item):
     logger.info("tvalacarta.channels.clantv episodios")
@@ -106,27 +63,24 @@ def episodios(item):
     itemlist = []
 
     # Descarga la página
-    data = scrapertools.cache_page(item.url)
+    url = item.url+"/videos.json"
+    data = scrapertools.cache_page(url)
+    json_object = jsontools.load_json(data)
+    #logger.info("json_object="+json_object)
+    json_items = json_object["page"]["items"]
 
-    # Extrae los capítulos
-    patron = '<div class="contenido-serie">(.*?)</div>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    logger.info("tvalacarta.channels.clantv encontrados %d episodios" % len(matches) )
-    if len(matches)==0:
-        return itemlist
-    data2 = matches[0]
-
-    itemlist = videos(item,data2)
-
-    # Añade el resto de páginas
-    patron = '<li class="siguiente"><a rel="next" title="Ir a la p&aacute;gina siguiente" href="([^"]+)">Siguiente</a></li>'
-    matches = re.compile(patron,re.DOTALL).findall(data)
-    if DEBUG: scrapertools.printMatches(matches)
-
-    if len(matches)>0:
-        match = matches[0]
-        item.url = urlparse.urljoin(item.url,match)
-        itemlist.extend(episodios(item))
+    for json_item in json_items:
+        title = json_item["longTitle"]
+        url = json_item["uri"]
+        thumbnail = item.thumbnail
+        if json_item["description"] is not None:
+            plot = json_item["description"]
+        else:
+            plot = ""
+        fanart = item.fanart
+        page = url
+        if (DEBUG): logger.info(" title=["+repr(title)+"], url=["+repr(url)+"], thumbnail=["+repr(thumbnail)+"] plot=["+repr(plot)+"]")
+        itemlist.append( Item(channel="rtve", title=title , action="play" , server="rtve", page=page, url=url, thumbnail=thumbnail, fanart=thumbnail, show=item.show , plot=plot , viewmode="movie_with_plot", folder=False) )
 
     from core import config
     if (config.get_platform().startswith("xbmc") or config.get_platform().startswith("boxee")) and len(itemlist)>0:
@@ -134,93 +88,75 @@ def episodios(item):
 
     return itemlist
 
-def ultimos_videos(item):
-    logger.info("tvalacarta.channels.clantv ultimos_videos")
+# Copiado de servers/rtve.py para poder actualizar el canal
+def play(item):
+    logger.info("tvalacarta.channels.clantv play")
 
     itemlist = []
 
-    # Descarga la página
-    data = scrapertools.cache_page(item.url)
-    itemlist = videos(item,data)
-    #http://www.rtve.es/infantil/components/TE_INFDEF/videos/videos-1.inc
-    #http://www.rtve.es/infantil/components/TE_INFDEF/videos/videos-2.inc
-    #...
-    current_page = scrapertools.find_single_match(item.url,"videos-(\d+).inc$")
-    next_page = str(int(current_page)+1)
-    next_page_url = item.url.replace("videos-"+current_page+".inc","videos-"+next_page+".inc")
-    itemlist.append( Item(channel=item.channel, title=">> Página siguiente", url=next_page_url, action="ultimos_videos", folder=True))
-
-    return itemlist
-
-def videos(item,data2):
-    logger.info("tvalacarta.channels.clantv videos")
-    logger.info("tvalacarta.channels.clantv data2="+data2)
-
+    # Extrae el código
+    logger.info("url="+item.url)
+    codigo = scrapertools.find_single_match(item.url,'http://.*?/([0-9]+)')
+    url=""
     itemlist = []
+    logger.info("assetid="+codigo)
 
-    patron = '<a rel="([^"]+)".*?href="([^"]+)"><img src="([^"]+)"[^>]+>(.*?)</a>'
-    matches = re.compile(patron,re.DOTALL).findall(data2)
-    scrapertools.printMatches(matches)
+    # Código sacado de PyDownTV, gracias @aabilio :)
+    # https://github.com/aabilio/PyDownTV2/blob/master/spaintvs/tve.py
+    # -- Método 24 Mayo 2013
+    videoID = codigo
+    logger.info("Probando método de 24 de uno de Mayo de 2013")
+    tipo = "videos"
+    url = "http://www.rtve.es/ztnr/movil/thumbnail/default/%s/%s.png" % (tipo, videoID)
 
-    # Extrae los items
-    for match in matches:
-        scrapedtitle = match[3]
-        scrapedtitle = scrapertools.unescape(scrapedtitle)
-        scrapedtitle = scrapertools.htmlclean(scrapedtitle)
-        scrapedtitle = unicode(scrapedtitle,"utf-8").capitalize().encode("utf-8")
-        
-        # La página del vídeo
-        scrapedpage = urlparse.urljoin(item.url,match[1])
-        scrapedpage = scrapedpage.replace("videos-juegos/videos","videos-juegos/#/videos")
-        
-        # Código de la serie
-        scrapedcode = match[0]
-        
-        # Url de la playlist
-        scrapedurl = "http://www.rtve.es/infantil/components/"+scrapedcode+"/videos.xml.inc"
-        scrapedthumbnail = urlparse.urljoin(item.url,match[2])
-        scrapedplot = ""
-        if (DEBUG): logger.info("scraped title=["+scrapedtitle+"], url=["+scrapedurl+"], page=["+scrapedpage+"] thumbnail=["+scrapedthumbnail+"]")
+    logger.info("Probando url:"+url)
+    logger.info("Manager default")    
+    from base64 import b64decode as decode
+    tmp_ = decode(scrapertools.cachePage(url))
+    if tmp_== "" :
+        url = "http://www.rtve.es/ztnr/movil/thumbnail/anubis/%s/%s.png" % (tipo, videoID)
+        tmp_ = decode(scrapertools.cachePage(url)) 
+        logger.info("Manager anubis") 
+    tmp = re.findall(".*tEXt(.*)#[\x00]*([0-9]*).*", tmp_)[0]
+    tmp = [n for n in tmp]
+    cyphertext = tmp[0]
+    key = tmp[1]
+    tmp = tmp = [0 for n in range(500)]
 
-        # Añade al listado
-        itemlist.append( Item(channel=CHANNELNAME, title=scrapedtitle , action="play" , server="directo", page=scrapedpage, url=scrapedurl, thumbnail=scrapedthumbnail, fanart=scrapedthumbnail, show=item.show , plot=scrapedplot , viewmode="movie_with_plot", folder=False) )
+    # Créditos para: http://sgcg.es/articulos/2012/09/11/nuevos-cambios-en-el-mecanismo-para-descargar-contenido-multimedia-de-rtve-es-2/
+    intermediate_cyphertext = ""
+    increment = 1
+    text_index = 0
+    while text_index < len(cyphertext):
+        text_index = text_index + increment
+        try: intermediate_cyphertext = intermediate_cyphertext + cyphertext[text_index-1]
+        except: pass
+        increment = increment + 1
+        if increment == 5: increment = 1
 
-    # Ahora extrae el argumento y la url del vídeo
-    dataplaylist = scrapertools.cachePage(scrapedurl)
-    
-    for episodeitem in itemlist:
-        partes = episodeitem.page.split("/")
-        code = partes[len(partes)-2]
-        patron  = '<video id="'+code+'".*?url="([^"]+)".*?'
-        patron += '<sinopsis>(.*?)</sinopsis>'
-        matches = re.compile(patron,re.DOTALL).findall(dataplaylist)
+    plaintext = ""
+    key_index = 0
+    increment = 4
+    while key_index < len(key):
+        key_index = key_index + 1
+        text_index = int(key[key_index-1]) * 10
+        key_index = key_index + increment
+        try: text_index = text_index + int(key[key_index-1])
+        except: pass
+        text_index = text_index + 1
+        increment = increment + 1
+        if increment == 5: increment = 1
+        try: plaintext = plaintext + intermediate_cyphertext[text_index-1]
+        except: pass
 
-        if len(matches)>0:
-            episodeitem.url = urlparse.urljoin(item.url,matches[0][0])
-            episodeitem.plot = matches[0][1]
+    urlVideo = plaintext
+    if urlVideo != "":
+        url_video = urlVideo.replace("www.rtve.es", "media5.rtve.es")
+    else:
+        logger.info("No se pudo encontrar el enlace de descarga")
+    url=urlVideo
+    logger.info("url="+url)
+
+    itemlist.append( Item(channel="rtve", title=title , action="play" , url=url, thumbnail=thumbnail, fanart=thumbnail, show=item.show , plot=plot ,folder=False) )
 
     return itemlist
-
-# Verificación automática de canales: Esta función debe devolver "True" si todo está ok en el canal.
-def test():
-    bien = True
-    
-    # El canal tiene estructura programas -> episodios -> play
-    menu_itemlist = mainlist(Item())
-
-    items_programas = programas(menu_itemlist[1])
-    if len(items_programas)==0:
-        print "No hay programas"
-        return False
-
-    items_episodios = episodios(items_programas[0])
-    if len(items_episodios)==0:
-        print "No hay episodios en "+items_programas[0].tostring()
-        return False
-
-    items_novedades = ultimos_videos(menu_itemlist[0])
-    if len(items_novedades)==0:
-        print "No hay nada en ultimos-episodios"
-        return False
-
-    return bien
