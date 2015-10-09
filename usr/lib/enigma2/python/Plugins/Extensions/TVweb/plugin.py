00e82e2858a5fd9839baefa66baf8e6de1027960
morser <morser123@hotmail.com>
2015-10-09 19:23:07 +0200

    version 1.0.18

--- usr/lib/enigma2/python/Plugins/Extensions/TVweb/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/TVweb/plugin.py
@@ -70,7 +70,7 @@ config.plugins.TVweb.imagecache = ConfigEnableDisable(default=True)
 config.plugins.TVweb.showadultcontent = ConfigYesNo(default=False)
 config.plugins.TVweb.showsecretcontent = ConfigYesNo(default=False)
 config.plugins.TVweb.downloadimages = ConfigYesNo(default=True)
-config.plugins.TVweb.version = NoSave(ConfigText(default="1.0.17"))
+config.plugins.TVweb.version = NoSave(ConfigText(default="1.0.18"))
 config.plugins.TVweb.resolution = ConfigSelection(default="360p", choices = ["240p", "360p", "480p", "720p", "1080p"])
 config.plugins.TVweb.freemem = ConfigInteger(default=10, limits=(1, 60))
 
